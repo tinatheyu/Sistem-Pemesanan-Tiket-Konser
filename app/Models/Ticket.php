a90766f45da089aa285b3ac5d2a569194fcfcf6b
tinatheyu <tinatheyu@gmail.com>
2025-06-08 21:43:31 +0700

    Deskripsi singkat perubahan

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
+    protected $fillable = [
+        'event_id',
+        'user_id',
+        'pembayaran_id'
+    ];
+
     public function event()
     {
         return $this->belongsTo(Event::class);
